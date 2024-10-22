ff0411cc3dc6cff4e0edcd97a6fef1d494429509
Marco Kuidja <kuidjamarco@gmail.com>
2024-10-23 00:16:19 +0100

    playing with plaid sdk

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Plaid from './Plaid'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <Plaid plaidPublicToken='GENERATED_PLAID-TOKEN' />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
